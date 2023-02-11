044ea7c460e66f8684478f61528018970d619186
Gabriel Wu <wuzihua@pku.edu.cn>
2023-02-11 13:41:25 +0800

    fix script

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 run:
-	~/.local/bin/julia --project=. script.jl
+	LOCAL=true ~/.local/bin/julia --project=. script.jl
 
 inspect:
 	cat output.p4c | pcregrep -M "CONTACTS\n.*"
@@ -12,7 +12,7 @@ run-hpc:
 prepare-hpc:
 	sed 's/archive.ubuntu.com/mirrors.pku.edu.cn/g' -i /etc/apt/sources.list && \
 	apt update && apt install -y xvfb && \
-	nohup Xvfb -s '-screen 0 1024x768x24' & && \
+	nohup Xvfb -s '-screen 0 1024x768x24' & \
 	python3 -m pip install jill && \
 	python3 -m jill install 1.9 --unstable --confirm && \
 	/opt/julias/julia-1.9/bin/julia --project=. -e 'using Pkg; Pkg.instantiate()'
--- script.jl
+++ script.jl
@@ -1,22 +1,18 @@
 import Pkg;
 Pkg.activate(".");
 
-# To run GLMakie on server
-ENV["DISPLAY"] = ":0"
+if !haskey(ENV, "LOCAL")
+    # To run GLMakie on server
+    ENV["DISPLAY"] = ":0"
+end
 
 tick = time_ns()
 @info "Loading packages..."
 using DEMPort
 @info "Loading time: $((time_ns() - tick) * 1e-9) s"
 
-# Do not do warm up locally
-if !haskey(ENV, "LOCAL")
-    @info "Warming up..."
-    DEMPort.solve("cfg/6_warmup.toml"; save_information = false)
+@info "Warming up..."
+DEMPort.solve("cfg/6_warmup.toml"; save_information = false)
 
-    @info "Running benchmark..."
-    DEMPort.solve("cfg/6.toml")
-else
-    @info "Running benchmark locally..."
-    DEMPort.solve("cfg/6_local.toml")
-end
+@info "Running benchmark..."
+DEMPort.solve("cfg/6.toml")
